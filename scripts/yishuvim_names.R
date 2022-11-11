e2e8aa916f7e7a741715ffc5e4b19e2d75305971
Matan <matanhakim@gmail.com>
2022-11-11 18:58:32 +0200

    Adds yishuvim names from Ido Lan and updates yishuvim names file accordingly

--- scripts/yishuvim_names.R
+++ scripts/yishuvim_names.R
@@ -19,5 +19,26 @@ yishuvim <- yishuvim %>%
     yishuv_id = str_pad(yishuv_id, width = 4, side = "left", pad = "0")
   )
 
+# Yishuvim from Ido Lan
+yishuvim_2 <- read_excel("yishuvim_names_files/yishuvim_ido_lan.xls", col_types = "text")
+
+yishuvim_2 <- yishuvim_2 %>% 
+  select(
+    yishuv_name_1 = yeshuv,
+    yishuv_name_2 = `fixed name`,
+    yishuv_id = semelyeshuv
+  ) %>% 
+  mutate(
+    yishuv_id = str_pad(yishuv_id, width = 4, side = "left", pad = "0")
+  ) %>% 
+  pivot_longer(!yishuv_id, names_to = "name", values_to = "yishuv_name") %>% 
+  select(!name) %>% 
+  distinct(yishuv_name, .keep_all = TRUE)
+
+# Join data frames together
+yishuvim <- yishuvim %>% 
+  bind_rows(yishuvim_2) %>% 
+  distinct(yishuv_name, .keep_all = TRUE)
+
 # Write the CSV file
 write_csv(yishuvim, "yishuv_names.csv")
